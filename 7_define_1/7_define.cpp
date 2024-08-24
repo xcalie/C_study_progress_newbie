0a88857a80ec322fae511f0c1e6e935532a41132
xcalie <2833186964@qq.com>
2024-08-25 02:49:27 +0800

    8.25

---
+++ 7_define_1/7_define.cpp
@@ -0,0 +1,14 @@
+﻿#include<stdio.h>
+
+int main(void)
+{
+	int number_1 = 1;
+	number_1 = number_1 << 31;
+	printf("printf int %d\n", number_1);
+
+	unsigned int number_2 = 1;
+	number_2 <<= 31;
+	printf("printf unsigned int %u\n", number_2);
+	
+	return 0;
+}
---
+++ 7_define_2/7_define_2.cpp
@@ -0,0 +1,20 @@
+﻿#include<stdio.h>
+
+int main() {
+
+	short number_3 = 1;
+	number_3 <<= 15;
+	printf("printf short %hd\n", number_3);
+
+	unsigned short number_4 = 1;
+	number_4 <<= 15;
+	printf("printf unsigned short %hu\n", number_4);
+
+	unsigned short color;
+
+	//16bit
+	color = 0b1111100000000000;
+	printf("RED : 0x%X\n", color);
+
+	return 0;
+}
