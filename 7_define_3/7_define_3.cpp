d2f1be5c242fd40d67a44742ebddc700adedee69
xcalie <2833186964@qq.com>
2024-08-30 19:33:31 +0800

    8.30

---
+++ 7_define_3/7_define_3.cpp
@@ -0,0 +1,22 @@
+﻿#include<stdio.h>
+
+int main(void)
+{
+	long number_1 = 1;
+	number_1 <<= 31;
+	printf("printf long %ld\n", number_1);
+
+	unsigned long number_3 = 1;
+	number_3 <<= 31;
+	printf("printf nusigned long %lu\n", number_3);
+
+	long long number_2 = 1;
+	number_2 <<= 63;
+	printf("printf long long %lld\n", number_2);
+
+	unsigned long long number_4 = 1;
+	number_4 <<= 63;
+	printf("printf unsigned long long %llu", number_4);
+
+	return 0;
+}
---
+++ 7_define_sizeof/7_define_sizeof.cpp
@@ -0,0 +1,19 @@
+﻿#include<stdio.h>
+
+int main(void)
+{
+	printf("printf size of short : %zu\n", sizeof(short));
+	printf("printf size of unsigned short : %zu\n", sizeof(unsigned short));
+
+	printf("printf size of int : %zu\n", sizeof(int));
+	printf("printf size of unsigned int : %zu\n", sizeof(unsigned int));
+
+	printf("printf size of long : %zu\n", sizeof(long));
+	printf("printf size of unsigned long : %zu\n", sizeof(unsigned long));
+
+	printf("printf size of long long : %zu\n", sizeof(long long));
+	printf("printf size of unsigned long long : %zu\n", sizeof(unsigned long long));
+
+
+	return 0;
+}
