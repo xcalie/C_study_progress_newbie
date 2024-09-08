eb6857bf3b650e8fec8a3c900b21cf7cf5e0ccd2
xcalie <2833186964@qq.com>
2024-09-08 23:07:55 +0800

    9.8

---
+++ 021_6_compare/021_6_compare.cpp
@@ -0,0 +1,26 @@
+﻿#include<stdio.h>
+#include<stdint.h>
+#include<inttypes.h>
+
+
+int main(void)
+{
+	int32_t number_1 = 32;
+	int32_t number_2 = 53;
+
+	printf("number 1 : %" PRId32 " , number 2 : %" PRId32 "\n", number_1, number_2);
+
+	bool small_1_with_2 = number_1 < number_2;
+	printf("number 1 < number 2 : %" PRId32 "\n", small_1_with_2);
+
+	bool big_1_with_2 = number_1 > number_2;
+	printf("number 1 > numebr 2 : %" PRId32 "\n", big_1_with_2);
+
+	bool big_or_equal_1_with_2 = number_1 >= number_2;
+	printf("number 1 >= number 2 : %" PRId32 "\n", big_or_equal_1_with_2);
+
+	bool equal_1_with_2 = number_1 == number_2;
+	printf("number 1 == number 2 : %" PRId32 "\n", equal_1_with_2);
+
+	return 0;
+}
---
+++ 021_7_compare_ternary/021_7_compare_ternary.cpp
@@ -0,0 +1,15 @@
+﻿#include<stdio.h>
+#include<stdint.h>
+#include<inttypes.h>
+
+int main(void)
+{
+	int32_t number_1 = 3232;
+	int32_t number_2 = 4344;
+	printf("number 1 : %" PRId32 " , number 2 : %" PRId32 "\n", number_1, number_2);
+
+	bool greater = number_1 > number_2;
+	printf("number 1 > number 2 ? : %s", greater ? "true" : "false");
+
+	return 0;
+}
