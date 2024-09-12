98fbb5df46f51b45285464a2b656928a6cf3e246
xcalie <2833186964@qq.com>
2024-09-12 21:08:18 +0800

    9.12

---
+++ 024_while_again/024_while_again.cpp
@@ -0,0 +1,32 @@
+﻿#include<stdio.h>
+#include<stdint.h>
+#include<inttypes.h>
+
+int main(void)
+{
+	const uint32_t SALES = 23;			// 售卖价格
+	uint32_t money = 0;					// 当前已投入金额
+
+	printf("请投入23元购买\n");
+
+	// 直到投入金额大于售价退出
+	while (money < SALES)
+	{
+		uint32_t ones;
+		scanf_s("%" PRIu32 "", &ones);
+		
+		if (ones == 1 || ones == 2 || ones == 5)
+		{
+			money += ones;
+			if (money < SALES) printf("投入成功，当前已投入 %" PRIu32 "元 ，还需要投入 %" PRIu32 "元\n", money, SALES - money);
+		}
+		else
+		{
+			printf("投入失败，面额非1/2/5元\n");
+		}
+	}
+
+	printf("支付成功，结算余额%" PRIu32 "元\n", money - SALES);
+
+	return 0;
+}
---
+++ 024_while_break/024_while_break.cpp
@@ -0,0 +1,22 @@
+﻿#include<stdio.h>
+#include<stdint.h>
+#include<inttypes.h>
+#include<stdbool.h>
+
+int main(void)
+{
+	uint32_t number;
+	uint32_t result = 0;
+	while (true)
+	{
+		scanf_s(" %" PRIu32 "", &number);
+		result += number;
+		if (number == 0)
+		{
+			break;
+		}
+	}
+	printf("%" PRIu32 "", result);
+
+	return 0;
+}
