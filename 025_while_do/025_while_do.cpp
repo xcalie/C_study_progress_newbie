4122ebe50b9ae16cd3ce98d04bd382415026784d
xcalie <2833186964@qq.com>
2024-09-13 14:42:42 +0800

    9.13

---
+++ 025_while_do/025_while_do.cpp
@@ -0,0 +1,26 @@
+﻿#include<stdio.h>
+#include<stdint.h>
+#include<inttypes.h>
+#include<stdbool.h>
+
+int main(void)
+{
+	uint32_t number = 1;
+	const uint32_t INDEX = 1;
+
+	while (number < INDEX)
+	{
+		number++;
+		printf("%" PRIu32 "\n", number);
+	}
+
+	number = 1;
+	do
+	{
+		number++;
+		printf("%" PRIu32 "\n", number);
+	} while (number < INDEX);
+	// do while至少会执行一次循环体
+
+	return 0;
+}
