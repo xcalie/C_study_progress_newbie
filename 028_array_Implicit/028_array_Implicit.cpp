c5dc432b06aa641d4bd10381c16e79d96d0ae96c
xcalie <2833186964@qq.com>
2024-09-17 23:28:08 +0800

    9.17

---
+++ 028_array_Implicit/028_array_Implicit.cpp
@@ -0,0 +1,21 @@
+﻿#include<stdio.h>
+#include<stdint.h>
+#include<inttypes.h>
+
+#define d32 int32_t
+
+int main(void)
+{
+	d32 number[] = {
+		2,4,5,4,3,5
+	};
+	printf("%" PRIu32 "", number[4]);
+	// 隐式定义，更具内容增加空间
+	/*
+		使用隐式定义时 
+		原先a[123] = {0};的方法下定义全部数组的方法会失效
+		仅有固定大小数组才可以全定义
+	*/
+
+	return 0;
+}
---
+++ 028_array_two_dimensional/028_array_two_dimensional.cpp
@@ -0,0 +1,25 @@
+﻿#include<stdio.h>
+#include<stdint.h>
+#include<inttypes.h>
+
+#define d32 int32_t
+
+int main(void)
+{
+	d32 number[3][3] = {
+		{0,1,2},
+		{0,1,2},
+		{0,1,2}
+	};
+	
+	for (d32 i = 0; i < 3; i++)
+	{
+		for (d32 j = 0; j < 3; j++)
+		{
+			printf("%" PRIu32 " ", number[i][j]);
+		}
+		printf("\n");
+	}
+
+	return 0;
+}
