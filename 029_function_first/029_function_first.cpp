1fd67010a400b44f481b7eb7c8aebfcaf03e0fed
xcalie <2833186964@qq.com>
2024-09-18 23:22:30 +0800

    9.18

---
+++ 029_function_first/029_function_first.cpp
@@ -0,0 +1,20 @@
+﻿#include<stdio.h>
+#include<stdint.h>
+#include<inttypes.h>
+
+#define u32 uint32_t
+
+void print(void)
+{
+	printf("------------------\n");
+}
+
+int main(void)
+{
+	for (u32 index = 1; index <= 5; index++)
+	{
+		print();
+	}
+
+	return 0;
+}
