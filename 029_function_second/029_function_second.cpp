483424eceaa096d7235496ffdc4744bd80e3ae53
xcalie <2833186964@qq.com>
2024-09-20 23:42:56 +0800

    9.20

---
+++ 029_function_second/029_function_second.cpp
@@ -0,0 +1,28 @@
+﻿#include<stdio.h>
+#include<stdint.h>
+#include<inttypes.h>
+
+#define u32 uint32_t
+
+void print(void);
+void speak(u32 number);
+
+
+int main(void)
+{
+	speak(3);
+	print();
+	speak(2);
+
+	return 0;
+}
+
+void print(void)
+{
+	printf("Hi\n");
+}
+
+void speak(u32 number)
+{
+	printf("%" PRIu32 "\n", number);
+}
