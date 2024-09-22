77afb6fca45d65307711cb8f6a1112b11abbf2ff
xcalie <2833186964@qq.com>
2024-09-22 23:35:50 +0800

    9.22

---
+++ 029_function_practice/029_function_practice.cpp
@@ -0,0 +1,18 @@
+﻿#include<stdio.h>
+#include<stdint.h>
+#include<inttypes.h>
+
+#define u32 uint32_t
+
+u32 calculate(u32 long_, u32 wide)
+{
+	return long_ * wide;
+}
+
+int main(void)
+{
+
+	printf("%" PRIu32 "\n", calculate(32, 24));
+
+	return 0;
+}
