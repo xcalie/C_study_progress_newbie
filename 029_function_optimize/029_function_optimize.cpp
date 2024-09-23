e31f489467935ac9aeceddcc1f87d4595c8d3ba3
xcalie <2833186964@qq.com>
2024-09-23 20:17:42 +0800

    9.23

---
+++ 029_function_optimize/029_function_optimize.cpp
@@ -0,0 +1,23 @@
+﻿#include<stdio.h>
+#include<stdint.h>
+#include<inttypes.h>
+
+#define u32 uint32_t
+
+bool judge_leap_year(u32 year);
+
+int main(void)
+{
+	u32 number_year;
+	scanf_s("%" SCNu32 "", &number_year);
+
+	printf("%s\n", judge_leap_year(number_year) ? "是润年" : "不是闰年");
+	
+
+	return 0;
+}
+
+bool judge_leap_year(u32 year)
+{
+	return ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0);
+}
