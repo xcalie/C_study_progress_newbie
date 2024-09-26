7a72f01abb0a7ec4114112cbef69ae45a8ceb004
xcalie <2833186964@qq.com>
2024-09-26 23:44:43 +0800

    9.26

---
+++ 031_function_annotation/031_function_annotation.cpp
@@ -0,0 +1,23 @@
+﻿#include<stdio.h>
+#include<stdint.h>
+#include<inttypes.h>
+
+#define u32 uint32_t
+#define d32 int32_t
+
+/**
+*	这个是标准函数注释的案例
+*	在每一行都有一个*
+*	会出现第一个有两个**的情况
+*/
+void process(void)
+{
+	printf("Hallo world\n");
+}
+
+int main(void)
+{
+	process();
+
+	return 0;
+}
---
+++ 033_address/033_address.cpp
@@ -0,0 +1,27 @@
+﻿#include<stdio.h>
+#include<stdint.h>
+#include<inttypes.h>
+
+#define u32 uint32_t
+#define d32 int32_t
+
+int main(void)
+{
+	d32 number = 2;
+	d32* address = &number;
+	printf("%p\n", &number);
+	printf("%p\n", address);
+	
+	printf("%d\n", number);
+	printf("%d\n", *address);
+	/*
+		作为指针可以通过 *xxx 对指针的地址进行指向
+		直接指向对应变量
+
+		此处
+			address == &number
+			*address == number
+	*/
+
+	return 0;
+}
