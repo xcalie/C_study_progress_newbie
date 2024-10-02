4f5749abe2d98f3230ded9c9092ae747c44e793a
xcalie <2833186964@qq.com>
2024-10-02 22:22:25 +0800

    10.2

---
+++ 100_scanf_newbie/100_scanf_newbie.cpp
@@ -0,0 +1,30 @@
+﻿#include<stdio.h>
+#include<stdint.h>
+#include<inttypes.h>
+
+#define u32 uint32_t
+#define d32 int32_t
+
+
+
+int main(void)
+{
+	d32 number_1, number_2;
+
+	scanf_s("%d %d", &number_1, &number_2);
+	printf("%d %d\n", number_1, number_2);
+
+
+	scanf_s("%d_%d", &number_1, &number_2);
+	printf("%d %d\n", number_1, number_2);
+	// 中间塞什么决定根据什么输入
+
+
+	d32* ptr_1 = &number_1;
+	d32* ptr_2 = &number_2;
+
+	scanf_s("%d %d", ptr_1, ptr_2);
+	printf("%d %d\n", number_1, number_2);
+
+	return 0;
+}
---
+++ 102_fix_struct_with_pointer/102_fix_struct_with_pointer.cpp
@@ -0,0 +1,45 @@
+﻿#include<stdio.h>
+#include<stdint.h>
+#include<inttypes.h>
+
+#define u32 uint32_t
+#define d32 int32_t
+
+typedef struct {
+	d32 day;
+	d32 month;
+	d32 year;
+}Date;
+
+void fix_1(Date* object);
+void fix_2(Date* object);
+
+int main(void)
+{
+	Date one_day = { 4, 5, 2024 };
+	printf("%" PRId32 "-%" PRId32 "-%" PRId32 "\n", one_day.year, one_day.month, one_day.day);
+
+	fix_1(&one_day);
+
+	printf("%" PRId32 "-%" PRId32 "-%" PRId32 "\n", one_day.year, one_day.month, one_day.day);
+
+	fix_2(&one_day);
+
+	printf("%" PRId32 "-%" PRId32 "-%" PRId32 "\n", one_day.year, one_day.month, one_day.day);
+
+	return 0;
+}
+
+
+// 两种指针访问方式
+void fix_1(Date* object) {
+	(object->day)++;
+	(object->month)++;
+	(object->year)++;
+}
+
+void fix_2(Date* object) {
+	(*object).day++;
+	(*object).month++;
+	(*object).year++;
+}
