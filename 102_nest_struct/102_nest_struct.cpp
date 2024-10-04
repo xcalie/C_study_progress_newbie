214198cd322f6ec884475f0a0e04eeae1975aac7
xcalie <2833186964@qq.com>
2024-10-04 20:34:49 +0800

    10.4

--- 102_nest_struct/102_nest_struct.cpp
+++ 102_nest_struct/102_nest_struct.cpp
@@ -2,8 +2,9 @@
 #include<stdint.h>
 #include<inttypes.h>
 
-#define d32 int32_t
 #define u32 uint32_t
+#define d32 int32_t
+
 
 typedef struct {
 	d32 address;
@@ -19,24 +20,16 @@ typedef struct {
 	d32 personality;
 } Person;
 
-Person get_start(void);
 
 int main(void)
 {
-	Person one_people = get_start();
-	// 通过这方式进行赋值更安全
+	Person one_people = { { 439033, 100012, 12306 }, 24, 175, 85, 22 };
+
 	printf("%" PRId32 " %" PRId32 " %" PRId32 "\n%" PRId32 " %" PRId32 " %" PRId32 " %" PRId32 "\n", one_people.money.address, one_people.money.id, one_people.money.strength, one_people.age, one_people.height, one_people.weight, one_people.personality);
 
 	Person* ptr = &one_people;
-	printf("%" PRId32 " %" PRId32 "", ptr -> money.address, ptr -> age);
+	printf("%" PRId32 " %" PRId32 "", ptr->money.address, ptr->age);
 	// 如此应用
 
 	return 0;
-}
-
-
-Person get_start(void)
-{
-	Person start = { { 439033, 100012, 12306 }, 24, 175, 85, 22 };
-	return start;
 }
