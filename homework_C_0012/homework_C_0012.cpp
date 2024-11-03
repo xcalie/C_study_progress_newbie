ba50b5fb4cdd821933c4062a1bb5949d153ecde3
xcalie <2833186964@qq.com>
2024-11-03 21:03:24 +0800

    11.3

---
+++ homework_C_0012/homework_C_0012.cpp
@@ -0,0 +1,13 @@
+﻿#include<stdio.h>
+int main(void)
+{
+	for (int i = 1; i <= 30; i++)
+	{
+		if (i % 3 != 0)
+		{
+			printf("%d ", i);
+		}
+	}
+
+	return 0;
+}
---
+++ homework_C_0013/homework_C_0013.cpp
@@ -0,0 +1,15 @@
+﻿#include<stdio.h>
+int main(void)
+{
+	int ans = 1;
+	int n = 0;
+	scanf_s("%d", &n);
+	for (int i = 1; i <= n; i++)
+	{
+		ans *= i;
+	}
+
+	printf("%d", ans);
+
+	return 0;
+}
