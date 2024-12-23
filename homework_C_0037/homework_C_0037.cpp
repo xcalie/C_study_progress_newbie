b9c3fe79567f1537dbcb23e440f169bbbd56db09
xcalie <2833186964@qq.com>
2024-12-23 17:46:49 +0800

    12.23

---
+++ homework_C_0037/homework_C_0037.cpp
@@ -0,0 +1,29 @@
+﻿#include<stdio.h>
+#include<string.h>
+
+char news[105];
+char a[100];
+
+int main(void)
+{
+    int index;
+    char x;
+
+	gets_s(a, 100);
+    scanf_s("%d %c", &index, &x);
+
+    int len = strlen(a);
+
+    for (int i = len - 1; i >= index; i--)
+        news[i + 1] = a[i];
+
+    news[index] = x;
+
+    for (int i = 0; i < index; i++)
+        news[i] = a[i];
+
+
+    printf("%s", news);
+
+    return 0;
+}
